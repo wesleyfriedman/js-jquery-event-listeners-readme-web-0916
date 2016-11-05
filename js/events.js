94469f13fb20bce04bd4e60b55e1d51fec15799d
wesleyfriedman <friedman.wesley@gmail.com>
2016-11-04 20:22:04 -0400

    Done

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
-//define functions here
+function getIt(){
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $(document).on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $(document).on('keydown', function(event){
+    var key = parseInt(event.detail || event.which)
+    if (key === 71)  {
+      alert('Yay! You pressed the right letter.')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+
+
+
 
 $(document).ready(function(){
 
-// call functions here
+getIt()
+frameIt()
+submitIt()
+pressIt()
 
 });
